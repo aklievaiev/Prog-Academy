aa2ff17d1b2a3081faa650cf71ef8097bf44ffe7
aklievaiev <klievaiev@gmail.com>
2022-08-12 22:26:20 +0300

    Homework #1

---
+++ HM#1.py
@@ -0,0 +1,35 @@
+# Exercise 1
+
+print('Hello World')
+
+# Exercise 2
+
+print('Hello Python')
+print('Hello ProgAcademy')
+print('Hello Artur')
+
+# Exercise 3
+
+length = float(input('Please, write length of rectangle: '))
+width = float(input('Please, write width of rectangle: '))
+print('The area of rectangle is ' + str(length*width) + ' square centimeters')
+
+# Exercise 4
+
+a = int(input('Write the first number: '))
+b = int(input('Write the second number: '))
+print(a+b)
+print(a-b)
+print(a/b)
+
+# Exercise 5
+
+radius = float(input('Please, write the radius of a circle: '))
+pi = 3.14159
+diameter = 2 * radius
+circumference = 2 * pi * radius
+area = pi * radius ** 2
+print(diameter)
+print(circumference)
+print(area)
+
